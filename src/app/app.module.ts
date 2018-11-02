66b9687b0295a2152306ee72a98c2b29cc5a160c
DESARROLLO3\Desarrollo3 <gabrieloros93@gmail.com>
2018-11-02 12:46:08 -0300

    creacion de componente mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { MapsComponent } from './components/maps/maps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot(
+      {
+      apiKey: 'AIzaSyC01tRXSJ7cf5_GpsyBjtjaYa3Zl9GjNQ0',
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/maps/maps.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+export class MapsComponent implements OnInit {
+  lat = 51.678418;
+  lng = 7.809007;
+  constructor() { }
+
+  ngOnInit() {
+  
+  }
+  
+
+}
